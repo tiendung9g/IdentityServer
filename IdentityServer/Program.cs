098caec4b2285775e8c49d976d2ef4d96a3ea867
dane.dung <dane.dung@itlvn.com>
2022-12-27 15:49:21 +0700

    add git demo

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,36 @@
+using IdentityServer.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+         .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
+        .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+        .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+        .AddTestUsers(InMemoryConfig.GetUsers())
+        .AddInMemoryClients(InMemoryConfig.GetClients())
+        .AddDeveloperSigningCredential(); //not something we want to use in a production environment;
+
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseIdentityServer();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+//app.MapGet("/", () => "Idenity Server");
 
 app.Run();
