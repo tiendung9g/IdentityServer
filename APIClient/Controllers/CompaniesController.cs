9f7de5291429c3985a32922365228875d98d976c
dane.dung <dane.dung@itlvn.com>
2022-12-27 17:31:09 +0700

    ver 1.0 finish

---
+++ APIClient/Controllers/CompaniesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public CompaniesController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var claims = User.Claims;
+
+                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+
+                return Ok(companiesDto);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ APIClient/Program.cs
@@ -0,0 +1,48 @@
+using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+builder.Services.AddAuthentication("Bearer")
+   .AddJwtBearer("Bearer", opt =>
+   {
+       opt.RequireHttpsMetadata = false;
+       opt.Authority = "https://localhost:5005";
+       opt.Audience = "companyApi";
+   });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("APIClient")));
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
